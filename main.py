ebce84cb5db79876fe208f6823240d74673b674a
Krushna Dode <krushnadode5@gmail.com>
2025-11-12 14:39:02 +0530

    add ioobj 11

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ class Account:
         print(self.__acc_pass)
 
 a1 =Account("12021","123@")
+a2 =Account("12022","123@")
+
 
 print(a1.acc_no)
 print(a1.reset_())
+
+print(a2.acc_no)
+print(a2.reset_())
